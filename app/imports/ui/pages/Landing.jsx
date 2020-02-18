7c519d219e12d12d3efea264d067a8f3614ad847
jkasuya <35503264+jkasuya@users.noreply.github.com>
2020-02-17 22:49:24 -1000

    edited landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -8,12 +8,12 @@ class Landing extends React.Component {
   render() {
     return (
 
-        <div class="landing-background">
+        <div>
           <Grid>
             <Grid.Row centered>
               <Grid.Column textAlign='center'>
-                <Header as='h1' inverted className='title'> Welcome to Never Alone!</Header>
-                <Header as='h3' inverted className='subheader'>Find the Right Study Group for You!</Header>
+                <Header as='h1' className='title'> Welcome to Never Alone!</Header>
+                <Header as='h3' className='subheader'>Find the Right Study Group for You!</Header>
               </Grid.Column>
             </Grid.Row>
 
@@ -23,19 +23,19 @@ class Landing extends React.Component {
 
           <Grid stackable centered columns={3}>
             <Grid.Column textAlign='center'>
-              <Icon size='huge' name='group' inverted/>
-              <Header as='h1' inverted>Individual Accounts</Header>
-              <Header as='h3' inverted>When a user creates an account, it will be personalized towards them</Header>
+              <Icon size='huge' name='group'/>
+              <Header as='h1'>Individual Accounts</Header>
+              <Header as='h3'>When a user creates an account, it will be personalized towards them</Header>
             </Grid.Column>
             <Grid.Column textAlign='center'>
-              <Icon size='huge' name='file text' inverted/>
-              <Header as='h1' inverted>View Study Groups</Header>
-              <Header as='h3' inverted>Find out information about different study groups</Header>
+              <Icon size='huge' name='file text'/>
+              <Header as='h1'>View Study Groups</Header>
+              <Header as='h3'>Find out information about different study groups</Header>
             </Grid.Column>
             <Grid.Column textAlign='center'>
-              <Icon size='huge' name='checked calendar' inverted/>
-              <Header as='h1' inverted>Create Study Group Schedules</Header>
-              <Header as='h3' inverted>Create study groups and schedule when they would meet</Header>
+              <Icon size='huge' name='checked calendar'/>
+              <Header as='h1'>Create Study Group Schedules</Header>
+              <Header as='h3'>Create study groups and schedule when they would meet</Header>
             </Grid.Column>
             <Grid.Row className='blankspace'>
             </Grid.Row>
@@ -45,7 +45,7 @@ class Landing extends React.Component {
             <Grid>
               <Grid.Row centered>
                 <Grid.Column textAlign='center'>
-                  <Header as='h1' inverted className='instruct'> To get started, login or register now</Header>
+                  <Header as='h1' className='instruct'> To get started, login or register now</Header>
                   <Button className='buttoning' as={NavLink} activeClassName="active" exact to="/signin" key='signin'>
                     Log In
                   </Button>

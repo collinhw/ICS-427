a34374f4ff90de65198c97b2062b9067d9d8ce2f
jkasuya <35503264+jkasuya@users.noreply.github.com>
2020-02-17 22:42:43 -1000

    created landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,63 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import { Grid, Icon, Header, Image, Button, Modal } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+        <div class="landing-background">
+          <Grid>
+            <Grid.Row centered>
+              <Grid.Column textAlign='center'>
+                <Header as='h1' inverted className='title'> Welcome to Never Alone!</Header>
+                <Header as='h3' inverted className='subheader'>Find the Right Study Group for You!</Header>
+              </Grid.Column>
+            </Grid.Row>
 
-          <Grid.Column width={8}>
-            TBD
-          </Grid.Column>
+            <Grid.Row className='blankspace'>
+            </Grid.Row>
+          </Grid>
 
-        </Grid>
+          <Grid stackable centered columns={3}>
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name='group' inverted/>
+              <Header as='h1' inverted>Individual Accounts</Header>
+              <Header as='h3' inverted>When a user creates an account, it will be personalized towards them</Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name='file text' inverted/>
+              <Header as='h1' inverted>View Study Groups</Header>
+              <Header as='h3' inverted>Find out information about different study groups</Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name='checked calendar' inverted/>
+              <Header as='h1' inverted>Create Study Group Schedules</Header>
+              <Header as='h3' inverted>Create study groups and schedule when they would meet</Header>
+            </Grid.Column>
+            <Grid.Row className='blankspace'>
+            </Grid.Row>
+          </Grid>
+
+          {!Meteor.userId() &&
+            <Grid>
+              <Grid.Row centered>
+                <Grid.Column textAlign='center'>
+                  <Header as='h1' inverted className='instruct'> To get started, login or register now</Header>
+                  <Button className='buttoning' as={NavLink} activeClassName="active" exact to="/signin" key='signin'>
+                    Log In
+                  </Button>
+                  <Button as={NavLink} activeClassName="active" exact to="/signup" key='signup'>Register Now</Button>
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row className='blankspace'>
+              </Grid.Row>
+            </Grid>
+          }
+        </div>
     );
   }
 }
